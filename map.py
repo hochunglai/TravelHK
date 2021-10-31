7e818f236324db0c3a21cf153c30d335fcf450e9
hochunglai <92888439+hochunglai@users.noreply.github.com>
2021-10-30 23:03:56 -0400

    Geojson of Hong Kong (TBD)

--- map.py
+++ map.py
@@ -1,5 +1,17 @@
 import folium
+import os
 
-m = folium.Map()
+
+m = folium.Map(location=[22.3193,114.1694], zoom_start=11, tiles='CartoDB Dark_Matter')
+
+outline= os.path.join('outline.json')
+
+folium.GeoJson(outline, name='outline').add_to(m)
+
+
+folium.TileLayer('CartoDB Dark_Matter').add_to(m)
+folium.TileLayer('CartoDB Positron').add_to(m)
+
+folium.LayerControl().add_to(m)
 
 m.save('map.html')
