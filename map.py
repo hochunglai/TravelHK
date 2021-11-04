e7b59a6ddf79937a12c01480a856d6bc22c9a1b9
hochunglai <92888439+hochunglai@users.noreply.github.com>
2021-11-04 14:25:36 -0400

    Zoom control Display Off, Added a marker

--- map.py
+++ map.py
@@ -2,7 +2,7 @@ import folium
 import os
 
 
-m = folium.Map(location=[22.3193,114.1694], zoom_start=11, tiles='CartoDB Dark_Matter')
+m = folium.Map(location=[22.3193,114.1694], zoom_start=11, tiles='CartoDB Dark_Matter', zoom_control=False)
 
 outline= os.path.join('outline.json')
 
@@ -13,5 +13,9 @@ folium.TileLayer('CartoDB Dark_Matter').add_to(m)
 folium.TileLayer('CartoDB Positron').add_to(m)
 
 folium.LayerControl().add_to(m)
+
+folium.Marker(
+    location=[22.3193,114.1694], popup="<i>Mt. Hood Meadows</i>"
+).add_to(m)
 
 m.save('map.html')
